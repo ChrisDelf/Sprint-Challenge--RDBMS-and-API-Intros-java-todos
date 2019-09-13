a1b4e868ce28e846bbd6eaa6aff9277c5a0e084c
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-13 12:36:03 -0400

    working on controllers

---
+++ javatodos/src/main/java/com/lambdaschool/javatodos/service/TodoService.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.javatodos.service;
+
+import com.lambdaschool.javatodos.model.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+    List<Todo> findAll();
+
+    Todo findTodoById(long id);
+
+    List<Todo> findByUserName(String username);
+
+    void delete(long id);
+
+    Todo save(Todo todo);
+
+    Todo update(Todo todo, long id);
+
+//    ArrayList<CountTodos> getCountTodos();
+
+}
---
+++ javatodos/src/main/java/com/lambdaschool/javatodos/service/TodoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.lambdaschool.javatodos.service;
+
+public class TodoServiceImpl {
+}
