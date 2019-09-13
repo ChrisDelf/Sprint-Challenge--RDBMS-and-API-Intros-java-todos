64ff0473bd36a28d4904af585ad28b3c35a11244
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-13 12:45:47 -0400

    add new user is working

--- javatodos/src/main/java/com/lambdaschool/javatodos/controller/UserController.java
+++ javatodos/src/main/java/com/lambdaschool/javatodos/controller/UserController.java
@@ -5,16 +5,18 @@ import com.lambdaschool.javatodos.model.User;
 import com.lambdaschool.javatodos.service.TodoService;
 import com.lambdaschool.javatodos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -24,8 +26,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private TodoService todoService;
+//    @Autowired
+//    private TodoService todoService;
 
     //Get
     //localhost:2019/users/mine
@@ -36,5 +38,25 @@ public class UserController {
         return new ResponseEntity<>(userService.findUserByName(authentication.getName()), HttpStatus.OK);
 
     }
+    //Post
+    //localhost:2019/users/
+    @PostMapping(value = "/", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User newuser) throws URISyntaxException
+    {
+        newuser =  userService.save(newuser);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{userid}")
+                .buildAndExpand(newuser.getUserid())
+                .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+
 
 }
