12da2d1a5a60094c45aa5dfeeb2c4e3d5a82fa1d
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-13 11:27:08 -0400

    Added the models

---
+++ javatodos/src/main/java/com/lambdaschool/javatodos/model/Todo.java
@@ -0,0 +1,57 @@
+package com.lambdaschool.javatodos.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="todos")
+
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid",
+            nullable = false)
+    @JsonIgnoreProperties({"todo", "hibernateLazyInitializer"})
+    private User user;
+
+    public Todo() {
+    }
+
+    public Todo(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
